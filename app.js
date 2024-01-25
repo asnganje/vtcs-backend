d086bbf0b333a39ec835e630095b32e06b2f749c
asnganje <abd.nganje@gmail.com>
2024-01-25 11:31:40 +0300

    pass connectDB to app

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const app = express();
+const connectDB = require('./server/db/connect')
+
 
 const port = process.env.PORT;
+const url = process.env.MONGO_URI;
 
-const start = () => {
+const start = async () => {
     try {
+        await connectDB(url)
         app.listen(port, ()=> {
                 console.log(`Server is quitely listening on port ${port}...`);
         })
