5591572360bf091ebc8c3564c9dfb5254239a555
asnganje <abd.nganje@gmail.com>
2024-01-25 12:34:11 +0300

    add user creation, login and logout logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,13 +1,60 @@
-const createUser = (req,res) => {
-    res.send('Create user')
+const User = require("../models/User")
+
+const createUser = async (req,res) => {
+    try {
+        const user = await User.create(req.body)
+        res.status(201).json({
+            status: 'success',
+            msg: user
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            msg: error
+        })
+    }
+
 }
 
-const login = (req,res) => {
-    res.send('Login user')
+const login = async (req,res) => {
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+    if(user) {
+    const isValid = (user.comparePassword(password))
+    if(isValid) {
+        res.status(200).json({
+            status: 'success',
+            msg:user 
+        })
+    } else {
+        res.status(404).json({
+            status: 'fail',
+            msg: 'Incorrect password'
+        })
+    }
+    } else {
+        res.status(404).json({
+            status: 'fail',
+            msg: 'User not found'
+        })
+    }
+    
 }
 
-const logout = (req,res) => {
-    res.send('Logout user')
+const logout = async (req,res) => {
+    try {
+        const user = User.findOne(req.body)
+        res.status(200).json({
+            status: 'success',
+            msg: user
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            msg: error
+        })
+    }
 }
 
 module.exports = {createUser, login, logout}
